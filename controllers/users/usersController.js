77e8ce04caddda0ead26a9fb8f567afb842f1141
Joshua Garren <garrenjl@gmail.com>
2023-09-16 12:50:35 -0500

    Follow and Unfollow

--- controllers/users/usersController.js
+++ controllers/users/usersController.js
@@ -97,6 +97,129 @@ const getUser = async (req, res) => {
     }
 };
 
+const whoViewedProfile = async (req, res, next) => {
+    try {
+        //user of profile being viewed
+        const user = await User.findById(req.params.id);
+
+        //user viewing profile
+        const userWhoViewed = await User.findById(req.userAuth);
+
+        if (!user) {
+            return next(appError('User not found', 403));
+        }
+
+        if (!userWhoViewed) {
+            return next(appError('User viewed not found', 403));
+        }
+
+        if (user && userWhoViewed) {
+            //check if viewer has already viewed the profile
+            const userAlreadyViewed = user.viewedBy.find(viewer => viewer.toString() === userWhoViewed._id.toString());
+
+            if (userAlreadyViewed) {
+                return next(appError('User already viewed profile', 403));
+            } else {
+                //add new viewer to user's viewers and save
+                user.viewedBy.push(userWhoViewed._id);
+                await user.save();
+
+                res.json({
+                    data: 'You have viewed this profile.'
+                });
+            }
+        }
+    } catch (error) {
+        next(appError(error.message));
+    }
+}
+
+const followUser = async (req, res, next) => {
+    try {
+        //user to follow
+        const user = await User.findById(req.params.id);
+
+        //user following
+        const userWhoFollowed = await User.findById(req.userAuth);
+
+        if (!user) {
+            return next(appError('User to follow not found', 403));
+        }
+
+        if (!userWhoFollowed) {
+            return next(appError('User following not found', 403));
+        }
+
+        if (user && userWhoFollowed) {
+            //check if user is already following
+            const userAlreadyFollowing = user.followers.find(follower => follower.toString() === userWhoFollowed._id.toString());
+
+            if (userAlreadyFollowing) {
+                return next(appError('User already followed', 403));
+            } else {
+                //add new follower to user's followers
+                user.followers.push(userWhoFollowed._id);
+
+                //add user to following user's f0ollowed list
+                userWhoFollowed.following.push(user._id);
+
+                await user.save();
+                await userWhoFollowed.save();
+
+                res.json({
+                    data: 'You have followed this user'
+                });
+            }
+        }
+    } catch (error) {
+        next(appError(error.message));
+    }
+};
+
+
+const unfollowUser = async (req, res, next) => {
+    try {
+        //user to unfollow
+        const user = await User.findById(req.params.id);
+
+        //user unfollowing
+        const userWhoUnFollowed = await User.findById(req.userAuth);
+
+        if (!user) {
+            return next(appError('User to unfollow not found', 403));
+        }
+
+        if (!userWhoUnFollowed) {
+            return next(appError('User unfollowing not found', 403));
+        }
+
+        if (user && userWhoUnFollowed) {
+            //check if user is already following
+            const userAlreadyFollowing = user.followers.find(follower => follower.toString() === userWhoUnFollowed._id.toString());
+
+            if (!userAlreadyFollowing) {
+                return next(appError('User is not followed', 403));
+            } else {
+                //add new follower to user's followers
+                user.followers = user.followers.filter(follower => follower.toString() === userWhoUnFollowed._id);
+
+                await user.save();
+
+                //add user to following user's f0ollowed list
+                userWhoUnFollowed.following = userWhoUnFollowed.following.filter(follower => follower.toString() === user._id);
+                
+                await userWhoUnFollowed.save();
+
+                res.json({
+                    data: 'You have unfollowed this user'
+                });
+            }
+        }
+    } catch (error) {
+        next(appError(error.message));
+    }
+};
+
 const getAllUsers = async (req, res) => {
     try {
         res.json({
@@ -130,18 +253,18 @@ const updateUser = async (req, res) => {
     }
 };
 
-const uploadProfilePhoto = async (req, res) => {
+const uploadProfilePhoto = async (req, res, next) => {
     console.log(req.file);
     try {
         //find the user to update
         const userToUpdate = await User.findById(req.userAuth);
 
         if (!userToUpdate) {
-            return next(appErr('User not found', 403));
+            return next(appError('User not found', 403));
         }
 
         if (userToUpdate.isBlocked) {
-            return next(appErr('The account is blocked', 403));
+            return next(appError('The account is blocked', 403));
         }
 
         if (req.file) {
@@ -156,7 +279,7 @@ const uploadProfilePhoto = async (req, res) => {
                     new: true
                 }
             );
-        
+
             res.json({
                 data: 'You have successfully updated your profile photo'
             });
@@ -171,6 +294,9 @@ module.exports = {
     registerUser,
     loginUser,
     getUser,
+    whoViewedProfile,
+    followUser,
+    unfollowUser,
     getAllUsers,
     deleteUser,
     updateUser,
--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const storage = require('../../config/cloudinary');
-const { registerUser, loginUser, getUser, getAllUsers, deleteUser, updateUser, uploadProfilePhoto } = require('../../controllers/users/usersController');
+const { registerUser, loginUser, getUser, getAllUsers, deleteUser, updateUser, uploadProfilePhoto, whoViewedProfile, followUser, unfollowUser } = require('../../controllers/users/usersController');
 //const isLoggedIn = require('../../middleware/isLoggedIn');
 const multer = require('multer');
 const userRouter = express.Router();
@@ -18,6 +18,9 @@ userRouter.post('/login', loginUser);
 //GET/api/v1/users/profile/:id
 userRouter.get('/profile', isLoggedIn, getUser);
 
+//GET/api/v1/profile-viewers/:id
+userRouter.get('/profile-viewers/:id', isLoggedIn, whoViewedProfile);
+
 //GET/api/v1/users
 userRouter.get('/', getAllUsers);
 
@@ -29,6 +32,13 @@ userRouter.put('/:id', updateUser);
 
 //POST/api/v1/users/uploadPhoto
 userRouter.post('/uploadPhoto', isLoggedIn, upload.single('profile'), uploadProfilePhoto);
+
+//GET/api/v1/users/follow/:id
+userRouter.get('/follow/:id', isLoggedIn, followUser);
+
+//GET/api/v1/users/unfollow/:id
+userRouter.get('/unfollow/:id', isLoggedIn, unfollowUser);
+
 
 
 module.exports = userRouter;
